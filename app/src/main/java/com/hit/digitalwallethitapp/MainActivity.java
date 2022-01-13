05712e852231af607a23d084e6de87a9d47ae2f4
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-01-13 12:57:11 +0530

    Create a spinner Section Update

--- app/src/main/java/com/hit/digitalwallethitapp/MainActivity.java
+++ app/src/main/java/com/hit/digitalwallethitapp/MainActivity.java
@@ -10,23 +10,12 @@ import com.hit.digitalwallethitapp.ContactMembers.Data;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner_members;
-    private MembersAdapter adapter;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner_members =findViewById(R.id.spinner_mambers);
-
-
-
-        adapter = new MembersAdapter(MainActivity.this, Data.getMembersList());
-        spinner_members.setAdapter(adapter);
 
 
 
--- app/src/main/java/com/hit/digitalwallethitapp/SendMoneyScreen.java
+++ app/src/main/java/com/hit/digitalwallethitapp/SendMoneyScreen.java
@@ -8,16 +8,24 @@ import android.widget.Spinner;
 
 import com.hit.digitalwallethitapp.ContactMembers.Data;
 
+
 public class SendMoneyScreen extends AppCompatActivity  {
 
 
+    private Spinner spinner_members;
 
+
+    private MembersAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_money_screen);
 
         //create to Kasun
+
+
+        adapter = new MembersAdapter(SendMoneyScreen.this, Data.getMembersList());
+        spinner_members.setAdapter(adapter);
 
 
 
