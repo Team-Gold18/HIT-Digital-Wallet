4b9a1fbb7f1b93f4437793884fb2f110226cc09e
Kavinda <kavinda.sandakelum20@gmail.com>
2022-01-13 13:09:06 +0530

    withdraw account align part

--- app/src/main/java/com/hit/digitalwallethitapp/MainActivity.java
+++ app/src/main/java/com/hit/digitalwallethitapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.hit.digitalwallethitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
