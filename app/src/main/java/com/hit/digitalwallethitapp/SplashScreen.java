254b86e9e9d1dd7b15bb74aaac59aec4f370d898
nipunsampathperera <184128F@uom.lk>
2022-01-18 17:35:02 +0530

    navigation settings

--- app/src/main/java/com/hit/digitalwallethitapp/SplashScreen.java
+++ app/src/main/java/com/hit/digitalwallethitapp/SplashScreen.java
@@ -47,7 +47,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent i = new Intent(SplashScreen.this,TopUpScreen.class);
+                Intent i = new Intent(SplashScreen.this,BalanceScreen.class);
                 startActivity(i);
 
                 finish();
